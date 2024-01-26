0afd47afb9bdcf809f849f5a20e0434e3bc47ce0
gooseatTOKYO <hiromi.karizumi.0418@gmail.com>
2024-01-26 23:38:32 +0900

    test

---
+++ src/test/java/com/techacademy/controller/LogoutControllerTest.java
@@ -0,0 +1,14 @@
+package com.techacademy.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LogoutControllerTest {
+
+    @Test
+    void test() {
+        fail("まだ実装されていません");
+    }
+
+}
