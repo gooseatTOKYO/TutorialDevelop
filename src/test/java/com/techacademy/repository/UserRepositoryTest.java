899ba564a74f7ea929d00e669ed1109f3ed2f20f
gooseatTOKYO <hiromi.karizumi.0418@gmail.com>
2024-01-29 18:38:00 +0900

    test

--- src/test/java/com/techacademy/repository/UserRepositoryTest.java
+++ src/test/java/com/techacademy/repository/UserRepositoryTest.java
@@ -3,15 +3,13 @@ package com.techacademy.repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.techacademy.entity.User;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
+
 class UserRepositoryTest {
     private final UserRepository userRepository;
 
